af1cb501535dd59dc3c7b245bd52923e8bc5d279
RumanaB <rumana.badar@gmail.com>
2018-12-13 15:36:01 +0530

    Student class added
    Student's question is displayed

---
+++ BasicApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BasicApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Github!");
+            Student student = new Student();
+            student.StudentId = 1;
+            student.StudentName = "Hexagon";
+            student.Question = "Will Nasa save Iron Man?";
+            Console.WriteLine(student.Question);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BasicApp1/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicApp1
+{
+    class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public string Question { get; set; }
+
+    }
+}
